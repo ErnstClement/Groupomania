6a5e2733dce3a408d42c23f94e00bba325e1ec85
ErnstClement <75867922+ErnstClement@users.noreply.github.com>
2022-07-06 00:31:33 +0200

    ajout des photos et créations infrastrucutre

---
+++ groupomania/src/components/Banner.js
@@ -0,0 +1,12 @@
+import "../styles/Banner.css";
+
+function Banner() {
+  return (
+    <div className="g-banner">
+      {" "}
+      <h1>Groupomania</h1>
+    </div>
+  );
+}
+
+export default Banner;
