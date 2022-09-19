15d4f13dad226355fc6be4b599b8b1a281219831
ErnstClement <75867922+ErnstClement@users.noreply.github.com>
2022-09-19 13:26:12 +0200

    ajout axios login signup

--- groupomania/frontend/src/components/App.js
+++ groupomania/frontend/src/components/App.js
@@ -1,43 +1,23 @@
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-} from "react-router-dom";
 import "../styles/App.css";
 import "../styles/Auth.css";
 import "../styles/Form.css";
-import FormMail from "./FormMail";
-import FormPassword from "./FormPassword";
-import Loading from "./Loading";
 import Login from "./Login";
 import Signup from "./Signup";
+import Loading from "./Loading";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="main">
-        <Loading />
-        <div className="form-data">
-          <ul>
-            <li>
-              <Link to="/"> Accueil</Link>
-            </li>
-            <li>
-              <Link to="/login"> Se connecter</Link>
-            </li>
-            <li>
-              <Link to="/signup"> Inscription</Link>
-            </li>
-          </ul>
-        </div>
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="main">
+      <Loading />
+
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/login" element=<Login /> />
+          <Route exact path="/signup" element=<Signup /> />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- groupomania/frontend/src/components/Signup.js
+++ groupomania/frontend/src/components/Signup.js
@@ -1,15 +1,53 @@
-import React from "react";
-import FormMail from "./FormMail";
-import FormPassword from "./FormPassword";
-import Validate from "./Validate";
+import React, { useState } from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+import axios from "axios";
+const baseUrl = "http://localhost:3000/api/auth/signup";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const postData = () => {
+    console.log(postData);
+    console.log("email: " + email);
+    console.log("password: " + password);
+    axios
+      .post(baseUrl, {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(({ response }) => {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.headers);
+      });
+  };
+
   return (
     <div className="signup-container">
       <h1>Bienvenue sur la page d'inscription !</h1>;
-      <FormMail />
-      <FormPassword />
-      <Validate />
+      <Form className="create-form">
+        <Form.Field>
+          <label>Email :</label>
+          <input
+            placeholder="Entrez votre Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Mot de passe :</label>
+          <input
+            placeholder="Entrez votre mot de passe"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Field>
+        <Button onClick={postData} type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
